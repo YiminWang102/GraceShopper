12d1c11214741e6bf2be74eeb3cc1d53bc89dee8
Yi Min Wang <yimin@Yis-MacBook-Pro.local>
2017-06-05 17:21:32 -0400

    baseline product

---
+++ server/db/models/product.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+module.exports = db.define('product', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://i.redd.it/uh4wjeypg46y.jpg',
+    validate: {
+      isUrl: true
+    }
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  category: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+})
