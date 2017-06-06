0a214bcc8da032d132306c3521c0b7fa30143f8b
Yi Min Wang <yimin@Yis-MacBook-Pro.local>
2017-06-06 12:35:34 -0400

    added routes for user and a resetPassword on the user model

--- server/api/users.js
+++ server/api/users.js
@@ -2,8 +2,54 @@ const router = require('express').Router();
 const User = require('../db').model('user');
 module.exports = router;
 
+router.param('userId', (req, res, next, id) => {
+  User.findById(id)
+    .then(user => {
+      if(!user) {
+        const err = Error('User not found');
+        err.status = 404;
+        throw err;
+      }
+      req.user = user;
+      next();
+      return null;
+    })
+    .catch(next);
+});
+
 router.get('/', (req, res, next) => {
   User.findAll()
     .then(users => res.json(users))
+    .catch(next);
+});
+
+router.get('/:userId', (req, res, next) => {
+  res.json(req.user);
+});
+
+router.post('/', (req, res, next) => {
+  User.create(req.body)
+    .then( user => {
+      res.status(201).json(user);
+    })
+    .catch(next);
+});
+
+//TODO make sure only admins can hit this route
+//TODO maybe users can hit this route to change their password (we'll see)
+router.put('/:userId', (req, res, next) => {
+  req.user.update(req.body)
+    .then( () => {
+      res.sendStatus(200);
+    })
+    .catch(next);
+});
+
+//TODO make sure only admins can hit this route
+router.delete('/:userId', (req, res, next) => {
+  req.user.destroy()
+    .then( () => {
+      res.sendStatus(200);
+    })
     .catch(next);
 });
