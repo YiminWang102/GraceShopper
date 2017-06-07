e46522986414fbf1cb00760e4435becb8b95767a
YiminWang102 <ywang5991@gmail.com>
2017-06-07 10:43:18 -0400

    Update index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const User = require('./user');
 const Product = require('./product');
 const Review = require('./review');
@@ -7,9 +6,9 @@ const Order = require('./order');
 Review.belongsTo(User); // Will add a userId attribute to Review to hold the primary key value for User
 Review.belongsTo(Product);// Will add a productId attribute to Review to hold the primary key value for Product
 Product.hasMany(Review); //  will get the accessors getReviews and setReviews
-=======
+
 require('./user');
 require('./product');
 require('./review');
 require('./order');
->>>>>>> master
+
