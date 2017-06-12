c0ccd57314effaa03e492cda92acfa8d888ecabd
Brian Hong <brianhong@users.noreply.github.com>
2017-06-12 18:37:00 -0400

    Create CartContainer.js

--- client/containers/CartContainer.js
+++ client/containers/CartContainer.js
@@ -10,7 +10,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    dispatchPlaceOrder: event => {
+    onClick: event => {
       dispatch(placeOrder(event.target.button.value))
       alert("Your order has been placed!")
     }
